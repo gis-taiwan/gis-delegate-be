3af7233831aab5fbc2180c3eacae22591783e11c
louisexpc <louis60201@gmail.com>
2024-08-11 15:59:08 +0800

    2024 version

--- backend/src/routes/schedule.ts
+++ backend/src/routes/schedule.ts
@@ -5,25 +5,25 @@ const router = Router();
 
 router.get("/verify/:giscode", async (req, res) => {
   try {
-    // check giscode
-    if (!/^[A-Z][0-9]{5}$/.test(req.params.giscode)) {
+    // Check giscode format
+    if (!/^(AI|SF|SC)\d{3}$/.test(req.params.giscode)) {
       res.status(200).send("Invalid format");
       return;
     }
 
-    // exec query
+    // Exec query
     const result = await pool.query(
       "SELECT * FROM schedule WHERE gis_code = $1",
       [req.params.giscode]
     );
 
-    // check query found
+    // Check query found
     if (result.rowCount === 0) {
       res.status(400).send("GIS code not found");
       return;
     }
 
-    //return result
+    // Return result
     res.status(200).send("GIS code found");
   } catch (err) {
     if (err instanceof Error) {
@@ -35,25 +35,25 @@ router.get("/verify/:giscode", async (req, res) => {
 
 router.get("/schedule/:giscode", async (req, res) => {
   try {
-    // check giscode
-    if (!/^[A-Z][0-9]{5}$/.test(req.params.giscode)) {
+    // Check giscode format
+    if (!/^(AI|SF|SC)\d{3}$/.test(req.params.giscode)) {
       res.status(200).send("Invalid format");
       return;
     }
 
-    // exec query
+    // Exec query
     const result = await pool.query(
       "SELECT * FROM schedule WHERE gis_code = $1",
       [req.params.giscode]
     );
 
-    // check query found
+    // Check query found
     if (result.rowCount === 0) {
       res.status(400).send("GIS code not found");
       return;
     }
 
-    //return result
+    // Return result
     res.status(200).json(result.rows[0]);
   } catch (err) {
     if (err instanceof Error) {
